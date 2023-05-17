4c07d19a28f6373cc94665b63ad197a98b4e8581
AjeySrini <ajeysrini@gmail.com>
2023-05-17 17:51:41 +0530

    final changes

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 var div = document.createElement("div");
 div.style.textAlign = "center";
 div.style.marginTop = "15px";
-document.body.style.backgroundColor = "lightblue";
+document.body.style.backgroundImage = "url('bgimg.jpg')";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
 
 var h1 = document.createElement("h1");
 h1.innerHTML = "Guess the Card";
-h1.style.textAlign = "center";
+h1.style.textAlign="center";
 h1.style.marginTop = "30px";
 
+
+h1.setAttribute("class","text-bg-primary p-3");
+
+
 var p=document.createElement("p");
-p.innerHTML="Both suit and value match 3 pts, if only one match one pts";
+p.innerHTML="Both Suit and Value matches 3 pts, Only Value matches 2pts, Only Suit matches 1pts";
+
+
 p.style.textAlign="center";
+
+p.setAttribute("class","text-bg-dark");
 
 
 var label = document.createElement("label");
 label.setAttribute("for", "suits");
-label.style.marginRight = "10px";
+label.setAttribute("class","text-bg-info p-2");
 label.innerHTML = "Choose suits :";
 
 var selectvalue = document.createElement("select");
 selectvalue.setAttribute("id", "suits");
 selectvalue.setAttribute("name", "suits");
-selectvalue.style.margin = "7px";
+selectvalue.setAttribute("class","btn btn-secondary dropdown-toggle");
+selectvalue.style.margin = "5px";
 
 function createoptionvalue(value1) {
     var opt = document.createElement("option");
@@ -42,13 +53,14 @@ label.append(selectvalue);
 /////////////////
 var label1 = document.createElement("label");
 label1.setAttribute("for", "cardvalue");
-label1.style.marginRight = "20px";
+label1.setAttribute("class","text-bg-info p-2");
 label1.innerHTML = "Choose cardvalue :";
 
 var selectvalue1 = document.createElement("select");
 selectvalue1.setAttribute("id", "cardvalue");
 selectvalue1.setAttribute("name", "cardvalue");
-selectvalue1.style.margin = "7px";
+selectvalue1.setAttribute("class","btn btn-secondary dropdown-toggle");
+selectvalue1.style.margin = "5px";
 
 var optionv1 = createoptionvalue("1");
 var optionv2 = createoptionvalue("2");
@@ -72,25 +84,38 @@ label1.append(selectvalue1);
 
 var subbutton = document.createElement("button");
 subbutton.setAttribute("type", "submit");
-subbutton.setAttribute("class", " btn btn-secondary");
+subbutton.setAttribute("class", "btn btn-dark");
+subbutton.style.marginTop = "5px";
 subbutton.innerHTML = "Guess it";
 
 subbutton.addEventListener("click", foo);
 var disp = document.createElement("div");
 disp.setAttribute("type", "text");
 disp.style.marginTop = "20px";
-disp.style.backgroundColor = "gray";
 
 var points1=document.createElement("span");
 points1.style.color="red";
 points1.style.textAlign="center";
 points1.style.fontSize="30px";
 points1.innerHTML=0;
+
+var count1=document.createElement("span");
+count1.style.color="black";
+count1.style.textAlign="center";
+count1.style.fontSize="30px";
+count1.innerHTML=0;
+
+var count2=document.createElement("span");
+count2.style.color="black";
+count2.style.textAlign="center";
+count2.style.fontSize="20px";
+count2.innerHTML="-----Total Count--:";
+
 var span=document.createElement("div");
 span.style.color="red";
 span.style.textAlign="center";
-span.style.fontSize="30px";
-span.style.backgroundColor="green";
+span.style.fontSize="20px";
+span.style.backgroundColor="aqua";
 span.innerHTML="Your Total Point is :"
 async function foo() {
     let suits = document.getElementById("suits").value;
@@ -104,24 +129,41 @@ async function foo() {
         
         if(suits==res1.cards[0].suit && cardvalue==res1.cards[0].value){
 
-            points1.innerHTML=parseInt(points1.innerHTML)+3;
-            span.append(points1);
+            points1.innerHTML=parseInt(points1.innerHTML)+5;
+            count1.innerHTML=parseInt(count1.innerHTML)+1;
+            span.append(points1,count2,count1);
             document.body.append(span);
-        }else if(suits==res1.cards[0].suit || cardvalue==res1.cards[0].value){
+        }else if(suits==res1.cards[0].suit ){
             points1.innerHTML=parseInt(points1.innerHTML)+1;
-            span.append(points1);
+            count1.innerHTML=parseInt(count1.innerHTML)+1;
+            span.append(points1,count2,count1);
+            document.body.append(span);
+        }else if(cardvalue==res1.cards[0].value){
+            points1.innerHTML=parseInt(points1.innerHTML)+2;
+            count1.innerHTML=parseInt(count1.innerHTML)+1;
+            span.append(points1,count2,count1);
+            document.body.append(span);
+        }else{
+            count1.innerHTML=parseInt(count1.innerHTML)+1;
+            span.append(points1,count2,count1);
             document.body.append(span);
         }
        
         
 
 if(suits==res1.cards[0].suit && cardvalue==res1.cards[0].value){
-        disp.innerHTML = `<span class="fw-bold fs-5 ">Match Perfectely</span><br>
-        <img src=${res1.cards[0].image}></img`;
-}else{
-    disp.innerHTML = `<span class="fw-bold fs-5 ">Not Matched -- Random pick card : Suit -- ${res1.cards[0].suit} and Value  -- ${res1.cards[0].value}  </span><br>
+        disp.innerHTML = `<span class="fw-bold fs-5 text-bg-success">Perfect Match -- Random pick card : Suit -- ${res1.cards[0].suit} and Value  -- ${res1.cards[0].value}  </span><br>
+        <img src=${res1.cards[0].image}></img>;
+        <img src="wow.jpg"></img>`;
+}else if(suits==res1.cards[0].suit || cardvalue==res1.cards[0].value){
+    disp.innerHTML = `<span class="fw-bold fs-5 text-bg-warning">One got Matched -- Random pick card : Suit -- ${res1.cards[0].suit} and Value  -- ${res1.cards[0].value}  </span><br>
+    <img src=${res1.cards[0].image}></img>;
+    <img style="border-radius:60%" src="soclose.png"></img>`;
+}
+else{
+    disp.innerHTML = `<span class="fw-bold fs-5 text-bg-danger">Not Matched -- Random pick card : Suit -- ${res1.cards[0].suit} and Value  -- ${res1.cards[0].value}  </span><br>
     <img src=${res1.cards[0].image}></img>
-    <img src="gameover.jpg"></img>`;
+    <img style="border-radius:50%" src="gameover.jpg"></img>`;
 }
     } catch {
         disp.innerHTML = ` <img src=${res1.cards[0].image}></img>`;
